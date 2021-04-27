d2887a3bae2bceb8c82c942bfe1ded53e8c77555
Plonkio <60223717+Plonkio@users.noreply.github.com>
2021-04-27 19:50:13 +0200

    clock javascript

--- Javascript/Clock.js
+++ Javascript/Clock.js
@@ -1,13 +1,13 @@
-function startTime() {
+function startClock() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('Clock').innerHTML =
+    document.getElementById("Clock").innerHTML =
     h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 1000);
+    var t = setTimeout(startClock, 1000);
   }
   function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
